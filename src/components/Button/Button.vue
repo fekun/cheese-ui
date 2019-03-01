462307ee69bc7cdef1b50b62bf1f49b8b5e9ea22
翁焕胜 <1031377840@qq.com>
2019-03-01 15:15:55 +0800

    feat: add Button component

---
+++ src/components/Button/Button.vue
@@ -0,0 +1,45 @@
+<template>
+  <button
+    @click="handleClick"
+    :disabled="disabled"
+    :class="[
+      'ch-button',
+      type ? `ch-button-${type}` : '',
+      size ? `ch-button-${size}` : '',
+      {
+        'is-disabled': disabled
+      }
+    ]"
+    >
+    <slot></slot>
+  </button>
+</template>
+
+<script>
+export default {
+  name: 'Button',
+  props: {
+    type: {
+      type: String,
+      default: 'default'
+    },
+    size: {
+      type: String,
+      default: 'small'
+    },
+    disabled: {
+      type: Boolean,
+      default: false
+    }
+  },
+  methods: {
+    handleClick (evt) {
+      this.$emit('click', evt)
+    }
+  }
+}
+</script>
+
+<style scoped lang='scss'>
+  @import './index.scss';
+</style>
